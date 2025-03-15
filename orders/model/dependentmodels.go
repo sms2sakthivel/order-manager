e8b8a0e413318c659eeb073bc3ee90b8b5c39fc0
Sakthivel Ganesan <Sms2sakthivel@outlook.com>
2025-03-15 18:49:24 +0530

    Remove user_id and user_name attributes from UserResponse model

--- orders/model/dependentmodels.go
+++ orders/model/dependentmodels.go
@@ -1,17 +1,17 @@
 package model
 
+
 type UserResponse struct {
-	ID       uint   `json:"user_id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Username string `json:"user_name"`
+  Name     string `json:"name"`
+  Email    string `json:"email"`
 }
 
+
 type ProductResponse struct {
-	ID          uint   `json:"product_id"`
-	Name        string `json:"product_name"`
-	Brand       string `json:"brand"`
-	Category    string `json:"category"`
-	SubCategory string `json:"sub_category"`
-	Price       uint   `json:"price"`
+  ID          uint   `json:"product_id"`
+  Name        string `json:"product_name"`
+  Brand       string `json:"brand"`
+  Category    string `json:"category"`
+  SubCategory string `json:"sub_category"`
+  Price       uint   `json:"price"`
 }
